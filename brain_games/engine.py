6a839b2c5f529fe73895a186793e031faf1c7408
Артём Пахолков <paholkov635@yandex.ru>
2024-01-15 00:09:45 +0300

    correct engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -14,6 +14,6 @@ def run_game(get_answer_and_question, instruction):
             print('Correct!')
         else:
             print(f''''{answer_user}' is wrong answer ;(. Correct answer was '{correct_answer}'.
-Let's try again, {name} ''')
+Let's try again, {name}!''')
             return
     print(f'Congratulations, {name}!')
